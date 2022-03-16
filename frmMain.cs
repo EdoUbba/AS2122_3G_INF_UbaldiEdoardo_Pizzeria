685bf2b4a3a6022f3cb72bce11cd7678111c0bbd
Edoardo Ubaldi <Edoubba@gmail.com>
2022-03-16 08:38:38 +0100

    programma completo

--- frmMain.cs
+++ frmMain.cs
@@ -30,6 +30,7 @@ namespace AS2122_3G_INF_UbaldiEdoardo_Pizzeria
                 lblMarg.Text = Convert.ToString(arrayPizza[0]);
                 lbl4stag.Text = Convert.ToString(arrayPizza[1]);
                 lblVerd.Text = Convert.ToString(arrayPizza[2]);
+                lstPizza.Items.Add($"{cmbPizza.Text} {arrayPizza[tipoPizza]}");
             }
         }
     }
