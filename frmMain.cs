f098ae99478927c70340c6f68ccd08dc3b204f68
Edoardo Ubaldi <Edoubba@gmail.com>
2022-03-16 08:33:36 +0100

    Stampa la quantità delle pizze in label

--- frmMain.cs
+++ frmMain.cs
@@ -4,6 +4,7 @@ namespace AS2122_3G_INF_UbaldiEdoardo_Pizzeria
     {
         int[] arrayPizza = new int[3];
         int tipoPizza;
+        int quantità;
         public frmMain()
         {
             InitializeComponent();
@@ -26,6 +27,9 @@ namespace AS2122_3G_INF_UbaldiEdoardo_Pizzeria
                         break;
                 }
                 arrayPizza[tipoPizza] += Convert.ToInt32(txtQuant.Text);
+                lblMarg.Text = Convert.ToString(arrayPizza[0]);
+                lbl4stag.Text = Convert.ToString(arrayPizza[1]);
+                lblVerd.Text = Convert.ToString(arrayPizza[2]);
             }
         }
     }
